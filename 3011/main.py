8d1c4db8aed15b92d2f6e19bf91ab32f1c938bcc
69070129-Patchayanan <69070129@kmitl.ac.th>
2026-07-20 11:58:22 +0700

    3011
    2

---
+++ 3011/main.py
@@ -0,0 +1,28 @@
+"""Colors"""
+
+def main():
+    """Primary"""
+    color1 = input()
+    color2 = input()
+
+    if color1 == "Red" and color2 == "Yellow":
+        print("Orange")
+    elif color1 == "Yellow" and color2 == "Red":
+        print("Orange")
+    elif color1 == "Red" and color2 == "Blue":
+        print("Violet")
+    elif color1 == "Blue" and color2 == "Red":
+        print("Violet")
+    elif color1 == "Yellow" and color2 == "Blue":
+        print("Green")
+    elif color1 == "Blue" and color2 == "Yellow":
+        print("Green")
+    elif color1 == "Red" and color2 == "Red":
+        print("Red")
+    elif color1 == "Yellow" and color2 == "Yellow":
+        print("Yellow")
+    elif color1 == "Blue" and color2 == "Blue":
+        print("Blue")
+    else:
+        print("Error")
+main()
